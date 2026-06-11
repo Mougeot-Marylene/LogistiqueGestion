c74d5b248c068a786a22d2fa89004c252a0fe57a
Marylene <marylene253925@gmail.com>
2026-06-11 08:49:24 +0200

    Création de la page Connexion et debut de la page accueil

--- LogistiqueGestion/Program.cs
+++ LogistiqueGestion/Program.cs
@@ -11,7 +11,8 @@ namespace LogistiqueGestion
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            //Application.Run(new Connexion());
+            Application.Run(new TableauDeBord());
         }
     }
 }
---
+++ LogistiqueGestion/TableauDeBord.Designer.cs
@@ -0,0 +1,415 @@
+﻿namespace LogistiqueGestion
+{
+    partial class TableauDeBord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TableauDeBord));
+            panel_navigation = new Panel();
+            tbl_logo = new TableLayoutPanel();
+            linkLabel1 = new LinkLabel();
+            tbl_menu = new TableLayoutPanel();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            pictureBox1 = new PictureBox();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            tblp_tableau_bord = new TableLayoutPanel();
+            tlp_logo = new TableLayoutPanel();
+            logo = new PictureBox();
+            tblp_pers = new TableLayoutPanel();
+            pers = new PictureBox();
+            lbl_pers_conn = new Label();
+            lbl_role = new Label();
+            tblp_menu = new TableLayoutPanel();
+            liklab_tab_bord = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            lbl_commandes = new Label();
+            lLab_comm_envoie = new LinkLabel();
+            lbll_comm_final = new LinkLabel();
+            linkLabel3 = new LinkLabel();
+            panel_navigation.SuspendLayout();
+            tbl_logo.SuspendLayout();
+            tbl_menu.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            tableLayoutPanel2.SuspendLayout();
+            tblp_tableau_bord.SuspendLayout();
+            tlp_logo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)logo).BeginInit();
+            tblp_pers.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pers).BeginInit();
+            tblp_menu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel_navigation
+            // 
+            panel_navigation.BackColor = Color.FromArgb(6, 27, 57);
+            panel_navigation.Controls.Add(tbl_logo);
+            panel_navigation.Controls.Add(tbl_menu);
+            panel_navigation.Location = new Point(3, 3);
+            panel_navigation.Name = "panel_navigation";
+            panel_navigation.Size = new Size(177, 440);
+            panel_navigation.TabIndex = 0;
+            // 
+            // tbl_logo
+            // 
+            tbl_logo.ColumnCount = 1;
+            tbl_logo.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tbl_logo.Controls.Add(linkLabel1, 0, 0);
+            tbl_logo.Location = new Point(3, 66);
+            tbl_logo.Margin = new Padding(0);
+            tbl_logo.Name = "tbl_logo";
+            tbl_logo.RowCount = 4;
+            tbl_logo.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tbl_logo.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tbl_logo.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tbl_logo.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tbl_logo.Size = new Size(168, 138);
+            tbl_logo.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.Anchor = AnchorStyles.Left;
+            linkLabel1.AutoSize = true;
+            linkLabel1.LinkColor = Color.White;
+            linkLabel1.Location = new Point(3, 9);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(60, 15);
+            linkLabel1.TabIndex = 0;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "linkLabel1";
+            linkLabel1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // tbl_menu
+            // 
+            tbl_menu.ColumnCount = 1;
+            tbl_menu.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tbl_menu.Controls.Add(tableLayoutPanel1, 0, 0);
+            tbl_menu.Location = new Point(0, 0);
+            tbl_menu.Name = "tbl_menu";
+            tbl_menu.RowCount = 1;
+            tbl_menu.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tbl_menu.Size = new Size(174, 60);
+            tbl_menu.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel1.Location = new Point(3, 3);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(168, 54);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(162, 48);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.AutoSize = true;
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 21.75F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 78.25F));
+            tableLayoutPanel2.Controls.Add(tblp_tableau_bord, 0, 0);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(0, 0);
+            tableLayoutPanel2.MinimumSize = new Size(800, 450);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 4;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 66F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 159F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 192F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel2.Size = new Size(800, 450);
+            tableLayoutPanel2.TabIndex = 0;
+            // 
+            // tblp_tableau_bord
+            // 
+            tblp_tableau_bord.BackColor = Color.FromArgb(6, 25, 57);
+            tblp_tableau_bord.ColumnCount = 1;
+            tblp_tableau_bord.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tblp_tableau_bord.Controls.Add(tlp_logo, 0, 0);
+            tblp_tableau_bord.Controls.Add(tblp_pers, 0, 2);
+            tblp_tableau_bord.Controls.Add(tblp_menu, 0, 1);
+            tblp_tableau_bord.Dock = DockStyle.Fill;
+            tblp_tableau_bord.Location = new Point(3, 3);
+            tblp_tableau_bord.Name = "tblp_tableau_bord";
+            tblp_tableau_bord.RowCount = 3;
+            tableLayoutPanel2.SetRowSpan(tblp_tableau_bord, 4);
+            tblp_tableau_bord.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tblp_tableau_bord.RowStyles.Add(new RowStyle(SizeType.Absolute, 339F));
+            tblp_tableau_bord.RowStyles.Add(new RowStyle(SizeType.Absolute, 42F));
+            tblp_tableau_bord.Size = new Size(168, 444);
+            tblp_tableau_bord.TabIndex = 2;
+            // 
+            // tlp_logo
+            // 
+            tlp_logo.ColumnCount = 1;
+            tlp_logo.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tlp_logo.Controls.Add(logo, 0, 0);
+            tlp_logo.Location = new Point(3, 3);
+            tlp_logo.Name = "tlp_logo";
+            tlp_logo.RowCount = 1;
+            tlp_logo.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tlp_logo.Size = new Size(162, 57);
+            tlp_logo.TabIndex = 0;
+            // 
+            // logo
+            // 
+            logo.Dock = DockStyle.Fill;
+            logo.Image = (Image)resources.GetObject("logo.Image");
+            logo.Location = new Point(3, 3);
+            logo.Name = "logo";
+            logo.Size = new Size(156, 51);
+            logo.SizeMode = PictureBoxSizeMode.Zoom;
+            logo.TabIndex = 0;
+            logo.TabStop = false;
+            // 
+            // tblp_pers
+            // 
+            tblp_pers.ColumnCount = 2;
+            tblp_pers.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tblp_pers.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 124F));
+            tblp_pers.Controls.Add(pers, 0, 0);
+            tblp_pers.Controls.Add(lbl_pers_conn, 1, 1);
+            tblp_pers.Controls.Add(lbl_role, 1, 0);
+            tblp_pers.Location = new Point(3, 405);
+            tblp_pers.Name = "tblp_pers";
+            tblp_pers.RowCount = 2;
+            tblp_pers.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tblp_pers.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tblp_pers.Size = new Size(162, 36);
+            tblp_pers.TabIndex = 2;
+            // 
+            // pers
+            // 
+            pers.Image = (Image)resources.GetObject("pers.Image");
+            pers.Location = new Point(3, 3);
+            pers.Name = "pers";
+            tblp_pers.SetRowSpan(pers, 2);
+            pers.Size = new Size(32, 22);
+            pers.SizeMode = PictureBoxSizeMode.Zoom;
+            pers.TabIndex = 0;
+            pers.TabStop = false;
+            // 
+            // lbl_pers_conn
+            // 
+            lbl_pers_conn.Anchor = AnchorStyles.Left;
+            lbl_pers_conn.AutoSize = true;
+            lbl_pers_conn.ForeColor = Color.White;
+            lbl_pers_conn.Location = new Point(41, 18);
+            lbl_pers_conn.Name = "lbl_pers_conn";
+            lbl_pers_conn.Size = new Size(34, 15);
+            lbl_pers_conn.TabIndex = 1;
+            lbl_pers_conn.Text = "Mary";
+            // 
+            // lbl_role
+            // 
+            lbl_role.AutoSize = true;
+            lbl_role.ForeColor = Color.White;
+            lbl_role.Location = new Point(41, 0);
+            lbl_role.Name = "lbl_role";
+            lbl_role.Size = new Size(46, 15);
+            lbl_role.TabIndex = 2;
+            lbl_role.Text = "Pickeur";
+            // 
+            // tblp_menu
+            // 
+            tblp_menu.ColumnCount = 1;
+            tblp_menu.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tblp_menu.Controls.Add(liklab_tab_bord, 0, 1);
+            tblp_menu.Controls.Add(linkLabel2, 0, 3);
+            tblp_menu.Controls.Add(lbl_commandes, 0, 2);
+            tblp_menu.Controls.Add(lLab_comm_envoie, 0, 4);
+            tblp_menu.Controls.Add(lbll_comm_final, 0, 5);
+            tblp_menu.Controls.Add(linkLabel3, 0, 6);
+            tblp_menu.Location = new Point(3, 66);
+            tblp_menu.Name = "tblp_menu";
+            tblp_menu.RowCount = 8;
+            tblp_menu.RowStyles.Add(new RowStyle(SizeType.Percent, 30.30303F));
+            tblp_menu.RowStyles.Add(new RowStyle(SizeType.Percent, 69.69697F));
+            tblp_menu.RowStyles.Add(new RowStyle(SizeType.Absolute, 35F));
+            tblp_menu.RowStyles.Add(new RowStyle(SizeType.Absolute, 22F));
+            tblp_menu.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tblp_menu.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tblp_menu.RowStyles.Add(new RowStyle(SizeType.Absolute, 28F));
+            tblp_menu.RowStyles.Add(new RowStyle(SizeType.Absolute, 8F));
+            tblp_menu.Size = new Size(162, 173);
+            tblp_menu.TabIndex = 1;
+            // 
+            // liklab_tab_bord
+            // 
+            liklab_tab_bord.ActiveLinkColor = Color.FromArgb(27, 94, 164);
+            liklab_tab_bord.Anchor = AnchorStyles.Left;
+            liklab_tab_bord.AutoSize = true;
+            liklab_tab_bord.LinkBehavior = LinkBehavior.NeverUnderline;
+            liklab_tab_bord.LinkColor = Color.White;
+            liklab_tab_bord.Location = new Point(3, 14);
+            liklab_tab_bord.Name = "liklab_tab_bord";
+            liklab_tab_bord.Size = new Size(92, 15);
+            liklab_tab_bord.TabIndex = 0;
+            liklab_tab_bord.TabStop = true;
+            liklab_tab_bord.Text = "Tableau de bord";
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.ActiveLinkColor = Color.FromArgb(27, 94, 164);
+            linkLabel2.Anchor = AnchorStyles.Left;
+            linkLabel2.AutoSize = true;
+            linkLabel2.Font = new Font("Segoe UI", 8F);
+            linkLabel2.LinkBehavior = LinkBehavior.NeverUnderline;
+            linkLabel2.LinkColor = Color.White;
+            linkLabel2.Location = new Point(13, 72);
+            linkLabel2.Margin = new Padding(13, 0, 3, 0);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(126, 13);
+            linkLabel2.TabIndex = 4;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Commande en attentes";
+            // 
+            // lbl_commandes
+            // 
+            lbl_commandes.Anchor = AnchorStyles.Left;
+            lbl_commandes.AutoSize = true;
+            lbl_commandes.ForeColor = Color.White;
+            lbl_commandes.Location = new Point(3, 43);
+            lbl_commandes.Name = "lbl_commandes";
+            lbl_commandes.Size = new Size(75, 15);
+            lbl_commandes.TabIndex = 2;
+            lbl_commandes.Text = "Commandes";
+            // 
+            // lLab_comm_envoie
+            // 
+            lLab_comm_envoie.ActiveLinkColor = Color.FromArgb(27, 94, 164);
+            lLab_comm_envoie.Anchor = AnchorStyles.Left;
+            lLab_comm_envoie.AutoSize = true;
+            lLab_comm_envoie.Font = new Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lLab_comm_envoie.LinkBehavior = LinkBehavior.NeverUnderline;
+            lLab_comm_envoie.LinkColor = Color.White;
+            lLab_comm_envoie.Location = new Point(13, 93);
+            lLab_comm_envoie.Margin = new Padding(13, 0, 3, 0);
+            lLab_comm_envoie.Name = "lLab_comm_envoie";
+            lLab_comm_envoie.Size = new Size(118, 13);
+            lLab_comm_envoie.TabIndex = 3;
+            lLab_comm_envoie.TabStop = true;
+            lLab_comm_envoie.Text = "Commande en envoie";
+            // 
+            // lbll_comm_final
+            // 
+            lbll_comm_final.ActiveLinkColor = Color.FromArgb(27, 94, 164);
+            lbll_comm_final.Anchor = AnchorStyles.Left;
+            lbll_comm_final.AutoSize = true;
+            lbll_comm_final.Font = new Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbll_comm_final.LinkBehavior = LinkBehavior.NeverUnderline;
+            lbll_comm_final.LinkColor = Color.White;
+            lbll_comm_final.Location = new Point(13, 117);
+            lbll_comm_final.Margin = new Padding(13, 0, 3, 0);
+            lbll_comm_final.Name = "lbll_comm_final";
+            lbll_comm_final.Size = new Size(111, 13);
+            lbll_comm_final.TabIndex = 5;
+            lbll_comm_final.TabStop = true;
+            lbll_comm_final.Text = "Commande finalisée";
+            // 
+            // linkLabel3
+            // 
+            linkLabel3.ActiveLinkColor = Color.FromArgb(27, 94, 164);
+            linkLabel3.Anchor = AnchorStyles.Left;
+            linkLabel3.AutoSize = true;
+            linkLabel3.LinkBehavior = LinkBehavior.NeverUnderline;
+            linkLabel3.LinkColor = Color.White;
+            linkLabel3.Location = new Point(3, 148);
+            linkLabel3.Margin = new Padding(3, 10, 3, 0);
+            linkLabel3.Name = "linkLabel3";
+            linkLabel3.Size = new Size(41, 15);
+            linkLabel3.TabIndex = 6;
+            linkLabel3.TabStop = true;
+            linkLabel3.Text = "Stocks";
+            // 
+            // TableauDeBord
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel2);
+            MinimumSize = new Size(816, 489);
+            Name = "TableauDeBord";
+            Text = "TableauDeBord";
+            panel_navigation.ResumeLayout(false);
+            tbl_logo.ResumeLayout(false);
+            tbl_logo.PerformLayout();
+            tbl_menu.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            tableLayoutPanel2.ResumeLayout(false);
+            tblp_tableau_bord.ResumeLayout(false);
+            tlp_logo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)logo).EndInit();
+            tblp_pers.ResumeLayout(false);
+            tblp_pers.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pers).EndInit();
+            tblp_menu.ResumeLayout(false);
+            tblp_menu.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TableLayoutPanel tlp_conteneur;
+        private Panel panel_navigation;
+        private TableLayoutPanel tbl_logo;
+        private LinkLabel linkLabel1;
+        private TableLayoutPanel tbl_menu;
+        private TableLayoutPanel tableLayoutPanel1;
+        private PictureBox pictureBox1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private TableLayoutPanel tblp_tableau_bord;
+        private TableLayoutPanel tlp_logo;
+        private PictureBox logo;
+        private TableLayoutPanel tblp_menu;
+        private TableLayoutPanel tblp_pers;
+        private PictureBox pers;
+        private Label lbl_pers_conn;
+        private Label lbl_role;
+        private LinkLabel liklab_comm_att;
+        private Label lbl_commandes;
+        private LinkLabel liklab_tab_bord;
+        private LinkLabel lLab_comm_envoie;
+        private LinkLabel linkLabel2;
+        private LinkLabel lbll_comm_final;
+        private LinkLabel linkLabel3;
+    }
+}
